23ad4e7f506f28f89904d7f356082f3a9ebbe35c
Ben Dowdell <ben.dowdell87@gmail.com>
2019-11-13 22:12:06 -0600

    Refactor with Black to Pep8

--- src/wedgebuilder.py
+++ src/wedgebuilder.py
@@ -6,8 +6,9 @@
 
 import numpy as np
 
+
 def earthmodel(rock_props):
-	"""
+    """
 	Input:
 	rock_props as a list of len 6 in Vp-Density pairs
 
@@ -20,157 +21,169 @@ def earthmodel(rock_props):
 	returns rc
 	"""
 
-	# define the initial earth model
-	duration, depth = 101, 240
-	model = 1 + np.tri(depth, duration, -depth//3, dtype=int)
-	model[:depth//3,:] = 0
+    # define the initial earth model
+    duration, depth = 101, 240
+    model = 1 + np.tri(depth, duration, -depth // 3, dtype=int)
+    model[: depth // 3, :] = 0
 
-	# Populate each layer of the earth model with specific rock properties
-	rocks = np.array(rock_props).reshape(3,2)
+    # Populate each layer of the earth model with specific rock properties
+    rocks = np.array(rock_props).reshape(3, 2)
 
-	# use fancy indexing to create an earth model where each layer of the model
-	# has Vp & Density at each location
-	earth = rocks[model]
+    # use fancy indexing to create an earth model where each layer of the model
+    # has Vp & Density at each location
+    earth = rocks[model]
 
-	# Calculate the acoustic impedance of each layer
-	imp = np.apply_along_axis(np.product, -1, earth)
+    # Calculate the acoustic impedance of each layer
+    imp = np.apply_along_axis(np.product, -1, earth)
 
-	# Finally, calculate the reflection coefficients for the interfaces between
-	# each layer and then return
-	rc = (imp[1:,:] - imp[:-1,:]) / (imp[1:,:] + imp[:-1,:])
+    # Finally, calculate the reflection coefficients for the interfaces between
+    # each layer and then return
+    rc = (imp[1:, :] - imp[:-1, :]) / (imp[1:, :] + imp[:-1, :])
 
-	return imp, rc
+    return imp, rc
+
 
 def wavelet(duration=0.100, dt=0.001, f=25):
-	"""
+    """
 	This function defines a Ricker wavelet to convolve with the earth model
 	reflection coefficients to build the tuning wedge.
 	returns w
 	"""
-	t = np.linspace(-duration/2, (duration-dt)/2, duration/dt)
-	w = (1.-2.*(np.pi**2)*(f**2)*(t**2))*np.exp(-(np.pi**2)*(f**2)*(t**2))
-	return w
+    t = np.linspace(-duration / 2, (duration - dt) / 2, duration / dt)
+    w = (1.0 - 2.0 * (np.pi ** 2) * (f ** 2) * (t ** 2)) * np.exp(
+        -(np.pi ** 2) * (f ** 2) * (t ** 2)
+    )
+    return w
+
 
 def get_wavelet_plot_parms(w):
-	start = int(len(w) / 2 *-1 -1)
-	stop = int(len(w) / 2)
-	num = len(w)
-	return np.linspace(start,stop,num)
+    start = int(len(w) / 2 * -1 - 1)
+    stop = int(len(w) / 2)
+    num = len(w)
+    return np.linspace(start, stop, num)
+
 
 def tuningwedge(rc, w):
-	"""
+    """
 	This function takes the reflection coefficients and convolves them with the
 	wavelet to produce a synthetic tuning wedge
 	returns synth
 	"""
 
-	synth = np.apply_along_axis(lambda t: np.convolve(t, w, mode='same'),
-		axis=0, arr=rc)
-	return synth
+    synth = np.apply_along_axis(
+        lambda t: np.convolve(t, w, mode="same"), axis=0, arr=rc
+    )
+    return synth
+
 
 def mask_rc(rc):
-	return np.ma.masked_equal(rc, 0)
+    return np.ma.masked_equal(rc, 0)
+
 
 def tuningcurve(rc, synth, rock_props):
-	"""
+    """
 	This function calculates the tuning curve
 	Returns: z, z_m, z_ft, z_tuning, z_tuning_m, z_tuning_ft, amp
 	"""
 
-	depth = 240
+    depth = 240
 
-	rocks = np.array(rock_props).reshape(3,2)
-	AI = np.apply_along_axis(np.product, -1, rocks)
+    rocks = np.array(rock_props).reshape(3, 2)
+    AI = np.apply_along_axis(np.product, -1, rocks)
 
-	# Determine the wedge thickness at each trace
-	# Initially we assume that the top RC is a decrease in impedance,
-	#negative value (trough) SEG normal polarity
-	if AI[1] < AI[0]:
-		top = np.apply_along_axis(np.nanargmin, 0, rc) + 1
-		base = np.apply_along_axis(np.nanargmax, 0, rc) + 1
-	else:
-		top = np.apply_along_axis(np.nanargmax, 0, rc) + 1
-		base = np.apply_along_axis(np.nanargmin, 0, rc) + 1
+    # Determine the wedge thickness at each trace
+    # Initially we assume that the top RC is a decrease in impedance,
+    # negative value (trough) SEG normal polarity
+    if AI[1] < AI[0]:
+        top = np.apply_along_axis(np.nanargmin, 0, rc) + 1
+        base = np.apply_along_axis(np.nanargmax, 0, rc) + 1
+    else:
+        top = np.apply_along_axis(np.nanargmax, 0, rc) + 1
+        base = np.apply_along_axis(np.nanargmin, 0, rc) + 1
 
-	# calculate the wedge thickness, z, in twt, m, & ft.
-	z = base - top
+    # calculate the wedge thickness, z, in twt, m, & ft.
+    z = base - top
 
-	# Determine the thickness at which synth has max amplitude
-	# This is the measured tuning thickness in TWT
-	z_tuning = np.nanargmax(abs(synth[np.nanmax(top),:]))
+    # Determine the thickness at which synth has max amplitude
+    # This is the measured tuning thickness in TWT
+    z_tuning = np.nanargmax(abs(synth[np.nanmax(top), :]))
 
-	# Determine the apparent thickness at which synth has max amplitude
-	# This represents what is seismically resolvable, in TWT
-	if AI[1] < AI[0]:
-		topApparent = np.apply_along_axis(np.nanargmin, 0, synth) + 1
-		baseApparent = np.apply_along_axis(np.nanargmax, 0, synth) + 1
+    # Determine the apparent thickness at which synth has max amplitude
+    # This represents what is seismically resolvable, in TWT
+    if AI[1] < AI[0]:
+        topApparent = np.apply_along_axis(np.nanargmin, 0, synth) + 1
+        baseApparent = np.apply_along_axis(np.nanargmax, 0, synth) + 1
 
-	else:
-		topApparent = np.apply_along_axis(np.nanargmax, 0, synth) + 1
-		baseApparent = np.apply_along_axis(np.nanargmin, 0, synth) + 1
+    else:
+        topApparent = np.apply_along_axis(np.nanargmax, 0, synth) + 1
+        baseApparent = np.apply_along_axis(np.nanargmin, 0, synth) + 1
 
-	z_apparent = baseApparent - topApparent
-	z_apparent[0] = z_apparent[1]
+    z_apparent = baseApparent - topApparent
+    z_apparent[0] = z_apparent[1]
 
-	# Extract the amplitude along the top of the wedge model
-	#amp = abs(synth[np.nanmax(top),:])
-	ampTop = abs(synth[depth//3,:])
-	amp = ampTop
-	#ampBase = abs(np.diag(synth, k=-depth//3))
+    # Extract the amplitude along the top of the wedge model
+    # amp = abs(synth[np.nanmax(top),:])
+    ampTop = abs(synth[depth // 3, :])
+    amp = ampTop
+    # ampBase = abs(np.diag(synth, k=-depth//3))
 
-	# calculate the tuning onset thickness
-	ampRef = abs(ampTop[-1]) # grabs amplitude "reference" when z >> z_tuning
-	ampPC = [ ((abs(x) - ampRef)/ampRef) for x in ampTop ] # calculates Percent Change in amp along wedge top
-	z_onset = (len(ampTop) - np.argwhere(np.flip(ampPC)>0.01)[0][0]) - 1
+    # calculate the tuning onset thickness
+    ampRef = abs(ampTop[-1])  # grabs amplitude "reference" when z >> z_tuning
+    ampPC = [
+        ((abs(x) - ampRef) / ampRef) for x in ampTop
+    ]  # calculates Percent Change in amp along wedge top
+    z_onset = (len(ampTop) - np.argwhere(np.flip(ampPC) > 0.01)[0][0]) - 1
 
-	return z, z_tuning, amp, z_apparent, z_onset
+    return z, z_tuning, amp, z_apparent, z_onset
+
 
 def tuningVLine(amp):
-	ampMin = np.nanmin(amp)
-	ampMax = np.nanmax(amp)
-	return ampMin, ampMax
+    ampMin = np.nanmin(amp)
+    ampMax = np.nanmax(amp)
+    return ampMin, ampMax
+
 
 def results_summary(inArr):
-	rock_props = inArr[0]
-	f_central = inArr[1]
-	z_tuning_meas_TWT = inArr[2] / 1000
-	z_onset_meas_TWT = inArr[3] / 1000
+    rock_props = inArr[0]
+    f_central = inArr[1]
+    z_tuning_meas_TWT = inArr[2] / 1000
+    z_onset_meas_TWT = inArr[3] / 1000
 
-	AI1 = rock_props[0] * rock_props[1] / 1000
-	AI2 = rock_props[2] * rock_props[3] / 1000
-	vp2 = rock_props[2]
-	AI3 = rock_props[4] * rock_props[5] / 1000
+    AI1 = rock_props[0] * rock_props[1] / 1000
+    AI2 = rock_props[2] * rock_props[3] / 1000
+    vp2 = rock_props[2]
+    AI3 = rock_props[4] * rock_props[5] / 1000
 
-	rc1 = (AI2 - AI1) / (AI2 + AI1)
-	rc2 = (AI3 - AI2) / (AI3 + AI2)
+    rc1 = (AI2 - AI1) / (AI2 + AI1)
+    rc2 = (AI3 - AI2) / (AI3 + AI2)
 
-	f_apparent = f_central * (np.pi/np.sqrt(6))
-	f_dom_simm = f_central * 1.3
+    f_apparent = f_central * (np.pi / np.sqrt(6))
+    f_dom_simm = f_central * 1.3
 
-	z_tuning_widess_TWT = 1 / f_apparent / 2
-	z_tuning_simm_TWT = 1 / f_dom_simm / 2
+    z_tuning_widess_TWT = 1 / f_apparent / 2
+    z_tuning_simm_TWT = 1 / f_dom_simm / 2
 
-	z_tuning_meas_m = z_tuning_meas_TWT / 2 * vp2
-	z_tuning_widess_m = z_tuning_widess_TWT / 2 * vp2
-	z_tuning_simm_m = z_tuning_simm_TWT / 2 * vp2
+    z_tuning_meas_m = z_tuning_meas_TWT / 2 * vp2
+    z_tuning_widess_m = z_tuning_widess_TWT / 2 * vp2
+    z_tuning_simm_m = z_tuning_simm_TWT / 2 * vp2
 
-	z_onset_widess_TWT = 1 / f_apparent
-	z_onset_simm_TWT = 1 / f_dom_simm
+    z_onset_widess_TWT = 1 / f_apparent
+    z_onset_simm_TWT = 1 / f_dom_simm
 
-	z_onset_meas_m = z_onset_meas_TWT / 2 * vp2
-	z_onset_widess_m = z_onset_widess_TWT / 2 * vp2
-	z_onset_simm_m = z_onset_simm_TWT / 2 * vp2
+    z_onset_meas_m = z_onset_meas_TWT / 2 * vp2
+    z_onset_widess_m = z_onset_widess_TWT / 2 * vp2
+    z_onset_simm_m = z_onset_simm_TWT / 2 * vp2
 
-	wlength_widess = vp2 / f_apparent
-	wlength_simm = vp2 / f_dom_simm
+    wlength_widess = vp2 / f_apparent
+    wlength_simm = vp2 / f_dom_simm
 
-	z_limit_widess_TWT = 1 / f_apparent / 4
-	z_limit_widess_m = z_limit_widess_TWT / 2 * vp2
+    z_limit_widess_TWT = 1 / f_apparent / 4
+    z_limit_widess_m = z_limit_widess_TWT / 2 * vp2
 
-	z_limit_simm_TWT = 1 / (2.31*f_dom_simm)
-	z_limit_simm_m = z_limit_simm_TWT / 2 * vp2
+    z_limit_simm_TWT = 1 / (2.31 * f_dom_simm)
+    z_limit_simm_m = z_limit_simm_TWT / 2 * vp2
 
-	summary = """Summary of Measured and Theoretical Values\n
+    summary = """Summary of Measured and Theoretical Values\n
 Layer 1 Acoustic Impedance: {} (m/s).(g/cm3)\n
 Layer 2 Acoustic Impedance: {} (m/s).(g/cm3)\n
 Layer 3 Acoustic Impedance: {} (m/s).(g/cm3)\n
@@ -189,17 +202,32 @@ Wavelength (F_apparent): {} m\n
 Wavelength (F_dom): {} m\n
 Theoretical limit of resolution (F_apparent / 8, Widess, 1973): {} sec TWT, {} m\n
 Theoretical limit of resolution (1/2.31*Fdom, Simm & Bacon, 2014): {} sec TWT, {}m
-	""".format(round(AI1,2), round(AI2,2), round(AI3,2), round(rc1,4),
-		round(rc2,4), f_central,
-		round(f_apparent,2), f_dom_simm,
-		round(z_tuning_meas_TWT,4), round(z_tuning_meas_m,1),
-		round(z_tuning_widess_TWT,4), round(z_tuning_widess_m,1),
-		round(z_tuning_simm_TWT,4), round(z_tuning_simm_m,1),
-		round(z_onset_meas_TWT,4), round(z_onset_meas_m,1),
-		round(z_onset_widess_TWT,4), round(z_onset_widess_m,1),
-		round(z_onset_simm_TWT,4), round(z_onset_simm_m,1),
-		round(wlength_widess,2), round(wlength_simm,2),
-		round(z_limit_widess_TWT,4), round(z_limit_widess_m,1),
-		round(z_limit_simm_TWT,4), round(z_limit_simm_m,1))
-	return summary
-
+	""".format(
+        round(AI1, 2),
+        round(AI2, 2),
+        round(AI3, 2),
+        round(rc1, 4),
+        round(rc2, 4),
+        f_central,
+        round(f_apparent, 2),
+        f_dom_simm,
+        round(z_tuning_meas_TWT, 4),
+        round(z_tuning_meas_m, 1),
+        round(z_tuning_widess_TWT, 4),
+        round(z_tuning_widess_m, 1),
+        round(z_tuning_simm_TWT, 4),
+        round(z_tuning_simm_m, 1),
+        round(z_onset_meas_TWT, 4),
+        round(z_onset_meas_m, 1),
+        round(z_onset_widess_TWT, 4),
+        round(z_onset_widess_m, 1),
+        round(z_onset_simm_TWT, 4),
+        round(z_onset_simm_m, 1),
+        round(wlength_widess, 2),
+        round(wlength_simm, 2),
+        round(z_limit_widess_TWT, 4),
+        round(z_limit_widess_m, 1),
+        round(z_limit_simm_TWT, 4),
+        round(z_limit_simm_m, 1),
+    )
+    return summary
